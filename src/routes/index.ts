9ab11f0ed87fa5da04c2c12ea917e5e668999b2c
RodrigoGCorrea <rodrigogc@id.uff.br>
2023-12-02 00:03:21 -0300

    feat: added productRouter and storageRouter to Router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { userRouter } from "./userRoutes";
 import { roomRouter } from "./roomRouter";
+import { productRouter } from "./productRouter";
+import { storageRouter } from "./storageRouter";
 
 export const router = Router();
 
 router.use("/users", userRouter);
 router.use("/rooms", roomRouter);
+router.use("/products", productRouter)
+router.use("/storage", storageRouter)
