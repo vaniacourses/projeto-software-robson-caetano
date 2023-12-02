6f4f676a5ce0eb41746dc71adab8ec50bcdff8b2
Kayalla <kayallapl@id.uff.br>
2023-12-02 17:47:15 -0300

    feat: appointment repository

---
+++ src/repositories/appointment/AppointmentRepositoryStrategy.ts
@@ -0,0 +1,15 @@
+import { Appointment, AppointmentStatus } from "@prisma/client";
+
+export interface AppointmentRepositoryStrategy {
+  getById(id: number): Promise<Appointment | null>;
+
+  create(
+    data: Pick<Appointment, "doctorId" | "patientId">,
+  ): Promise<Appointment>;
+
+  list(): Promise<Appointment[]>;
+
+  update(id: number, status: AppointmentStatus): Promise<Appointment>;
+
+  delete(id: number): Promise<void>;
+}
---
+++ src/repositories/appointment/DatabaseAppointmentRepositoryStrategy.ts
@@ -0,0 +1,52 @@
+import { PrismaClient, PrismaSingleton } from "~/config/database";
+import { AppointmentRepositoryStrategy } from "./AppointmentRepositoryStrategy";
+import { Appointment, AppointmentStatus } from "@prisma/client";
+
+export class DatabaseAppointmentRepositoryStrategy
+  implements AppointmentRepositoryStrategy
+{
+  private db: PrismaClient;
+
+  constructor() {
+    this.db = PrismaSingleton.getInstance();
+  }
+
+  public async getById(id: number): Promise<Appointment | null> {
+    return this.db.appointment.findUnique({
+      where: { id },
+      include: {
+        doctor: true,
+        patient: true,
+      },
+    });
+  }
+
+  public async create({
+    patientId,
+    doctorId,
+  }: Pick<Appointment, "doctorId" | "patientId">): Promise<Appointment> {
+    return this.db.appointment.create({
+      data: { status: AppointmentStatus.SCHEDULED, patientId, doctorId },
+    });
+  }
+
+  public async list(): Promise<Appointment[]> {
+    return this.db.appointment.findMany({
+      include: {
+        doctor: true,
+        patient: true,
+      },
+    });
+  }
+
+  public async update(
+    id: number,
+    status: AppointmentStatus,
+  ): Promise<Appointment> {
+    return this.db.appointment.update({ where: { id }, data: { status } });
+  }
+
+  public async delete(id: number): Promise<void> {
+    await this.db.appointment.delete({ where: { id } });
+  }
+}
