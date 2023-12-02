25e61e754904e9d4761a5b75d40eb253b163d745
RodrigoGCorrea <rodrigogc@id.uff.br>
2023-12-02 09:32:26 -0300

    feat: unprocessableEntityError

--- src/errors/http/httpErrorFactory.ts
+++ src/errors/http/httpErrorFactory.ts
@@ -2,6 +2,7 @@ import httpStatus from "http-status";
 import { ConflictError } from "~/errors/domain/ConflictError";
 import { NotFoundError } from "~/errors/domain/NotFoundError";
 import { HttpError } from "./HttpError";
+import { UnprocessableEntityError } from "../domain/UnprocessableEntityError";
 
 export class HttpErrorFactory {
   public static create(error: Error) {
@@ -11,6 +12,10 @@ export class HttpErrorFactory {
 
     if (error instanceof ConflictError) {
       return new HttpError(error.message, httpStatus.CONFLICT);
+    }
+
+    if (error instanceof UnprocessableEntityError) {
+      return new HttpError(error.message, httpStatus.UNPROCESSABLE_ENTITY)
     }
 
     return new HttpError(error.message, httpStatus.INTERNAL_SERVER_ERROR);
