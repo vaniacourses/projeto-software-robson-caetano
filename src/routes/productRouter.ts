ee8bd14b582e0a291b147f51e5771223f5eddacb
RodrigoGCorrea <rodrigogc@id.uff.br>
2023-12-02 00:02:26 -0300

    feat: added product router

---
+++ src/routes/productRouter.ts
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import httpStatus from "http-status";
+
+import { ListProductsController } from "~/controllers/product/ListProductsController";
+import { CreateProductController } from "~/controllers/product/CreateProductController";
+import { DeleteProductController } from "~/controllers/product/DeleteProductController";
+import { UpdateProductController } from "~/controllers/product/UpdateProductController";
+import { DatabaseProductRepositoryStrategy } from "~/repositories/product/DatabaseProductRepositoryStrategy";
+
+export const productRouter = Router();
+
+productRouter.get("/", async (_, res) => {
+    const products = await new ListProductsController(
+        new DatabaseProductRepositoryStrategy(),
+    ).listProducts();
+
+    res.json(products);
+});
+
+productRouter.post("/", async (req, res) => {
+    const product = await new CreateProductController(
+        new DatabaseProductRepositoryStrategy(),
+    ).createProduct({
+        name: req.body.name,
+    });
+
+    res.status(httpStatus.CREATED).json(product);
+});
+
+productRouter.put("/:id", async (req, res) => {
+    const product = await new UpdateProductController(
+        new DatabaseProductRepositoryStrategy(),
+    ).updateProduct({
+        id: Number(req.params.id),
+        name: req.body.name,
+    });
+
+    res.json(product);
+});
+
+productRouter.delete("/:id", async (req, res) => {
+    await new DeleteProductController(
+        new DatabaseProductRepositoryStrategy(),
+    ).deleteProduct({
+        id: Number(req.params.id),
+    });
+
+    res.sendStatus(httpStatus.NO_CONTENT);
+});
