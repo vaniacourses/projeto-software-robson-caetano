43bf9f6329e88b2240cf6cedb691dc8327e89843
RodrigoGCorrea <rodrigogc@id.uff.br>
2023-12-02 09:35:14 -0300

    feat: added product distribution router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,12 @@ import { userRouter } from "./userRoutes";
 import { roomRouter } from "./roomRouter";
 import { productRouter } from "./productRouter";
 import { storageRouter } from "./storageRouter";
+import { productDistributionRouter } from "./productDistibution";
 
 export const router = Router();
 
 router.use("/users", userRouter);
 router.use("/rooms", roomRouter);
-router.use("/products", productRouter)
 router.use("/storage", storageRouter)
+router.use("/products", productRouter)
+router.use("/product_distribution", productDistributionRouter)
