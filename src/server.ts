c2e9e8e9df48453284d07a2d44796244d9c3b655
Bruno Mello <bruno.barros.mello@gmail.com>
2023-12-03 12:28:21 -0300

    chore: cors setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import "express-async-errors";
 
 import express from "express";
+import cors from "cors";
 import { router } from "./routes";
 import { env } from "./config/env";
 import { errorMiddleware } from "./middlewares/expressErrorMiddleware";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
