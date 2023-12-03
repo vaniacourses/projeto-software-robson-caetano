34e9ea436b4fc48e206ba509d35e6816a65b8a19
RodrigoGCorrea <rodrigogc@id.uff.br>
2023-12-03 17:54:53 -0300

    feat: added room on controller

--- src/controllers/appointment/CreateAppointmentController.ts
+++ src/controllers/appointment/CreateAppointmentController.ts
@@ -2,11 +2,13 @@ import { AppointmentStatus, Role } from "@prisma/client";
 import { UnprocessableEntityError } from "~/errors/domain/UnprocessableEntityError";
 import { AppointmentRepositoryStrategy } from "~/repositories/appointment/AppointmentRepositoryStrategy";
 import { PatientRepositoryStrategy } from "~/repositories/patient/PatientRepositoryStrategy";
+import { RoomRepositoryStrategy } from "~/repositories/room/RoomRepositoryStrategy";
 import { UserRepositoryStrategy } from "~/repositories/user/UserRepositoryStrategy";
 
 interface Params {
   doctorId: number;
   patientId: number;
+  roomId: number;
 }
 
 export class CreateAppointmentController {
@@ -14,13 +16,20 @@ export class CreateAppointmentController {
     private usersRepository: UserRepositoryStrategy,
     private patientsRepository: PatientRepositoryStrategy,
     private appointmentRepository: AppointmentRepositoryStrategy,
-  ) {}
+    private roomRepository: RoomRepositoryStrategy
+  ) { }
 
-  async createAppointment({ doctorId, patientId }: Params) {
+  async createAppointment({ doctorId, patientId, roomId }: Params) {
     const doctor = await this.usersRepository.getById(doctorId);
 
     if (!doctor || doctor.role !== Role.DOCTOR) {
       throw new UnprocessableEntityError("Médico não encontrado");
+    }
+
+    const room = await this.roomRepository.getById(roomId);
+
+    if (!room) {
+      throw new UnprocessableEntityError("Sala não encontrada");
     }
 
     const patient = await this.patientsRepository.getById(patientId);
@@ -32,6 +41,7 @@ export class CreateAppointmentController {
     const appointment = await this.appointmentRepository.create({
       doctorId: doctor.id,
       patientId: patient.id,
+      roomId: room.id,
       status: AppointmentStatus.SCHEDULED,
     });
 
