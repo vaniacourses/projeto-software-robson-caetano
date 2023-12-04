fc3e0eb88ba037b941f56a42ed57c8dac544854c
Bruno Mello <bruno.barros.mello@gmail.com>
2023-12-04 19:56:40 -0300

    refactor: remove unused schema

--- src/schemas/user/createUserSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const UserSchema = z.object({
-  name: z.string().min(1, "Nome é obrigatório"),
-  email: z.string().email("Email inválido"),
-});
-
-export type UserType = z.infer<typeof UserSchema>;
